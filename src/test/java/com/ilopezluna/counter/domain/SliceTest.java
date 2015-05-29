d9a3c539c5fc891c0ecf4a92ef31232377fc1071
ilopezluna <ignasi.lopez.luna@gmail.com>
2015-05-29 16:33:51 +0200

    Adding BitSet

--- src/test/java/com/ilopezluna/counter/domain/SliceTest.java
+++ src/test/java/com/ilopezluna/counter/domain/SliceTest.java
@@ -12,7 +12,9 @@ import static org.junit.Assert.*;
  */
 public class SliceTest {
 
-    private final static int DEFAULT_INDEX = 1;
+    private final static int DEFAULT_INDEX = 0;
+    private static final int DEFAULT_POSITION = 0;
+    private static final int OTHER_POSITION = 1;
 
     @Before
     public void setUp() throws Exception {
@@ -28,5 +30,35 @@ public class SliceTest {
     public void testGetIndex() throws Exception {
         Slice slice = new Slice(DEFAULT_INDEX);
         Assert.assertEquals(DEFAULT_INDEX, slice.getIndex());
+    }
+
+    @Test
+    public void testHit() throws Exception {
+        Slice base = new Slice(DEFAULT_INDEX);
+        Slice against = new Slice(DEFAULT_INDEX+1);
+
+        Assert.assertEquals(base.count(), against.count());
+        base.hit(DEFAULT_POSITION);
+        Assert.assertNotEquals(base.count(), against.count());
+        against.hit(DEFAULT_POSITION);
+        Assert.assertEquals(base.count(), against.count());
+
+        base.hit(DEFAULT_POSITION);
+        Assert.assertEquals(base.count(), against.count());
+    }
+
+    @Test
+    public void testCount() throws Exception {
+        Slice slice = new Slice(DEFAULT_INDEX);
+        Assert.assertEquals(0, slice.count());
+
+        slice.hit(DEFAULT_POSITION);
+        Assert.assertEquals(1, slice.count());
+
+        slice.hit(DEFAULT_POSITION);
+        Assert.assertEquals(1, slice.count());
+
+        slice.hit(OTHER_POSITION);
+        Assert.assertEquals(2, slice.count());
     }
 }
