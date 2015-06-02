9b66c71a89c49eaa8325a3b4885ce6e748a1e2bd
ilopezluna <ignasi.lopez.luna@gmail.com>
2015-06-02 20:52:42 +0200

    Minor

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# counter
-[ ![Codeship Status for ilopezluna/counter](https://codeship.com/projects/4f321940-e862-0132-275b-6e9310b27ce4/status?branch=master)](https://codeship.com/projects/82790)
+# uniques
+[ ![Codeship Status for ilopezluna/uniques](https://codeship.com/projects/4f321940-e862-0132-275b-6e9310b27ce4/status?branch=master)](https://codeship.com/projects/82790)
--- src/main/java/com/ilopezluna/uniques/helper/DynamoDBHelper.java
+++ src/main/java/com/ilopezluna/uniques/helper/DynamoDBHelper.java
@@ -20,14 +20,28 @@ public class DynamoDBHelper {
     }
 
     public static Table createDataPointTable() throws InterruptedException {
-        ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
-        attributeDefinitions.add(new AttributeDefinition()
-                .withAttributeName(DataPointRepository.PRIMARY_KEY)
-                .withAttributeType("S"));
-        attributeDefinitions.add(new AttributeDefinition()
-                .withAttributeName(DataPointRepository.RANGE_KEY)
-                .withAttributeType("S"));
+        ArrayList<AttributeDefinition> attributeDefinitions = getAttributeDefinitions();
+        ArrayList<KeySchemaElement> keySchema = getKeySchemaElements();
+        CreateTableRequest request = getCreateTableRequest(attributeDefinitions, keySchema);
+        Table table = dynamoDB.createTable(request);
 
+        System.out.println("Waiting for " + DataPointRepository.TABLE_NAME
+                + " to be created...this may take a while...");
+        table.waitForActive();
+        return table;
+    }
+
+    private static CreateTableRequest getCreateTableRequest(ArrayList<AttributeDefinition> attributeDefinitions, ArrayList<KeySchemaElement> keySchema) {
+        return new CreateTableRequest()
+                    .withTableName(DataPointRepository.TABLE_NAME)
+                    .withKeySchema(keySchema)
+                    .withAttributeDefinitions(attributeDefinitions)
+                    .withProvisionedThroughput(new ProvisionedThroughput()
+                            .withReadCapacityUnits(1L)
+                            .withWriteCapacityUnits(1L));
+    }
+
+    private static ArrayList<KeySchemaElement> getKeySchemaElements() {
         ArrayList<KeySchemaElement> keySchema = new ArrayList<KeySchemaElement>();
         keySchema.add(new KeySchemaElement()
                 .withAttributeName(DataPointRepository.PRIMARY_KEY)
@@ -36,22 +50,18 @@ public class DynamoDBHelper {
         keySchema.add(new KeySchemaElement()
                 .withAttributeName(DataPointRepository.RANGE_KEY)
                 .withKeyType(KeyType.RANGE));
+        return keySchema;
+    }
 
-        CreateTableRequest request = new CreateTableRequest()
-                .withTableName(DataPointRepository.TABLE_NAME)
-                .withKeySchema(keySchema)
-                .withAttributeDefinitions(attributeDefinitions)
-                .withProvisionedThroughput(new ProvisionedThroughput()
-                        .withReadCapacityUnits(1L)
-                        .withWriteCapacityUnits(1L));
-
-        System.out.println("Issuing CreateTable request for " + DataPointRepository.TABLE_NAME);
-        Table table = dynamoDB.createTable(request);
-
-        System.out.println("Waiting for " + DataPointRepository.TABLE_NAME
-                + " to be created...this may take a while...");
-        table.waitForActive();
-        return table;
+    private static ArrayList<AttributeDefinition> getAttributeDefinitions() {
+        ArrayList<AttributeDefinition> attributeDefinitions = new ArrayList<AttributeDefinition>();
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName(DataPointRepository.PRIMARY_KEY)
+                .withAttributeType("S"));
+        attributeDefinitions.add(new AttributeDefinition()
+                .withAttributeName(DataPointRepository.RANGE_KEY)
+                .withAttributeType("S"));
+        return attributeDefinitions;
     }
 
 }
