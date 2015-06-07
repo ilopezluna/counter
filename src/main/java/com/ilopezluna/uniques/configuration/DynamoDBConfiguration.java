11d82661c5151aa96c531fec4969f943b9582dd0
ilopezluna <ignasi.lopez.luna@gmail.com>
2015-06-07 23:47:42 +0200

    Added properties

--- src/main/java/com/ilopezluna/uniques/configuration/DynamoDBConfiguration.java
+++ src/main/java/com/ilopezluna/uniques/configuration/DynamoDBConfiguration.java
@@ -4,6 +4,8 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 
+import static com.ilopezluna.uniques.configuration.UniquesProperties.getProperty;
+
 /**
  * Created by ignasi on 29/5/15.
  */
@@ -12,10 +14,14 @@ public class DynamoDBConfiguration {
     private static DynamoDB dynamoDB;
 
     private static void configure() {
-        AmazonDynamoDBClient client = new AmazonDynamoDBClient(new BasicAWSCredentials("xxx", "xxx"));
-        client.setEndpoint("http://localhost:8000");
+        AmazonDynamoDBClient client = new AmazonDynamoDBClient(getAwsCredentials());
+        client.setEndpoint(getProperty("endpoint"));
 
         dynamoDB = new DynamoDB(client);
+    }
+
+    private static BasicAWSCredentials getAwsCredentials() {
+        return new BasicAWSCredentials(getProperty("username"), getProperty("password"));
     }
 
     public static DynamoDB getDynamoDB() {
---
+++ src/main/java/com/ilopezluna/uniques/configuration/UniquesProperties.java
@@ -0,0 +1,45 @@
+package com.ilopezluna.uniques.configuration;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * Created by ignasi on 7/6/15.
+ */
+public class UniquesProperties {
+
+    private final static String resourceName = "config.properties";
+
+    private static Properties properties;
+
+    private static Properties getProperties() throws IOException {
+
+        if (properties != null) {
+            return properties;
+        }
+        InputStream input = null;
+        try {
+            properties = new Properties();
+            ClassLoader loader = Thread.currentThread().getContextClassLoader();
+            try(InputStream resourceStream = loader.getResourceAsStream(resourceName)) {
+                properties.load(resourceStream);
+            }
+            return properties;
+        }
+        finally {
+            if (input != null)
+                input.close();
+        }
+    }
+
+    public static String getProperty(String key) {
+        try {
+            return (String) getProperties().get(key);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
